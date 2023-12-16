215c817dfb47875d5036d65a7c69c54502a40c61
venir.dev <luca@venir.dev>
2023-12-16 02:11:21 +0100

    corrected favorite controller and mixin logic

--- lib/src/favorites/presentation/controllers/favorites_controller.dart
+++ lib/src/favorites/presentation/controllers/favorites_controller.dart
@@ -1,28 +1,59 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../items/domain/entities/item.dart';
-import '../../../shared/domain/interfaces/fn1.dart';
 import '../../domain/repositories/favorites_repository_interface.dart';
-import '../logic/favorite_controller_mixin.dart';
 
 part 'favorites_controller.g.dart';
 
 @riverpod
-final class FavoritesController extends _$FavoritesController with WithFavoritesMixin {
+final class FavoritesController extends _$FavoritesController {
   late FavoritesRepositoryInterface _repository;
 
   @override
-  Future<List<Item>> build(int page) {
+  Future<List<Item>> build() {
     _repository = ref.watch(favoritesRepositoryProvider);
-    return _repository.getFavorites(page: page);
+    return _repository.getFavorites(page: 0);
   }
 
-  @override
-  @protected
-  Fn1<Item, Future<int>> get addFavoriteCallback => _repository.removeFavorite;
+  Future<void> addPage() async {
+    throw UnimplementedError();
+  }
 
-  @override
-  @protected
-  Fn1<Item, Future<int>> get removeFavoriteCallback => _repository.removeFavorite;
+  /// Adds the given [item] to the favorites.
+  /// Triggers a side effect to add the favorite and updates the state accordingly.
+  Future<int> addFavorite(Item item) async {
+    final state = await future;
+    assert(
+      !state.contains(item),
+      "State mustn't have this item as a favorite to add it to the favorites",
+    );
+    assert(item.isNotFavorite, 'Item must not *be* favorite to add it to the favorites');
+
+    final favoriteId = await _repository.addFavorite(item);
+    await update((state) {
+      return [
+        ...state.map((f) => f.id == item.id ? f.copyWith(favoriteId: favoriteId) : f),
+      ];
+    });
+    return favoriteId;
+  }
+
+  /// Removes the given [item] from the favorites.
+  /// Triggers a side effect to remove the favorite and updates the state accordingly.
+  Future<int> removeFavorite(Item item) async {
+    final state = await future;
+    assert(
+      state.contains(item),
+      'State must have this item as a favorite to remove it from the favorites',
+    );
+    assert(item.isFavorite, 'Item must be favorite to remove it from the favorites');
+
+    final favoriteId = await _repository.removeFavorite(item);
+    await update((state) {
+      return [
+        ...state.map((f) => f.id == item.id ? f.copyWith(favoriteId: null) : f),
+      ];
+    });
+    return favoriteId;
+  }
 }
--- lib/src/search/presentation/controllers/search_controller.dart
+++ lib/src/search/presentation/controllers/search_controller.dart
@@ -1,10 +1,7 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../../favorites/presentation/controllers/favorites_controller.dart';
-import '../../../favorites/presentation/logic/favorite_controller_mixin.dart';
+import '../../../favorites/presentation/logic/with_favorites_mixin.dart';
 import '../../../items/domain/entities/item.dart';
-import '../../../shared/domain/interfaces/fn1.dart';
 import '../../domain/repositories/search_repository_interface.dart';
 
 part 'search_controller.g.dart';
@@ -18,14 +15,4 @@ final class SearchController extends _$SearchController with WithFavoritesMixin 
     _repository = ref.watch(searchRepositoryProvider);
     return _repository.getSearch();
   }
-
-  @override
-  @protected
-  Fn1<Item, Future<int>> get addFavoriteCallback =>
-      ref.read(favoritesControllerProvider(page).notifier).addFavorite;
-
-  @override
-  @protected
-  Fn1<Item, Future<int>> get removeFavoriteCallback =>
-      ref.read(favoritesControllerProvider(page).notifier).removeFavorite;
 }
